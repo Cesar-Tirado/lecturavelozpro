c127d8048eb6abe1c71a58ef795e47ebaf7a8bcd
Cesar-Tirado <cesar.tiradocent@gmail.com>
2024-08-26 17:29:25 -0500

    cambios de orden

--- js/custom.js
+++ js/custom.js
@@ -573,6 +573,20 @@ exercise6Btn.addEventListener('click', function () {
 
 
 			
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute('href'));
+
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+
+
 			
 // ------------------------------------------------- EJERCICIO DISTRACTORES ---------------------------------------------------
 	
